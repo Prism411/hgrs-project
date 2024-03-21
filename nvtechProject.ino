3c82be55f9f4d5f8f4e4ec4eebad88ae914df0db
Prism411 <jaderlouis@proton.me>
2024-03-21 14:23:08 -0300

    working on new logic

--- nvtechProject.ino
+++ nvtechProject.ino
@@ -1,29 +1,29 @@
-// ----------------------------------------------------------------------------
-// Project: Hand Gesture Recognition System (HGRS Project)
-// ----------------------------------------------------------------------------
-// University: Universidade Federal de Rondônia (UNIR)
-// ----------------------------------------------------------------------------
-// Students: 
-//   - Andrey Riça
-//   - Jáder Louis
-//   - Nicolas Sales
-//   - Samih Santos
-//   - Nicolas Sales
-//   - Wyllgner França
-// ----------------------------------------------------------------------------
-// Project Sponsor: NAVETECH
-// ----------------------------------------------------------------------------
-// Sponsorship: Samsung in partnership with Fundação de Apoio ao Desenvolvimento 
-// do Ensino, Ciência e Tecnologia do Estado de Rondônia (FUNDAPE)
-// ----------------------------------------------------------------------------
 
 void setup() {
-
-  // Initialize serial communication
+  pinMode(4, INPUT);
   Serial.begin(9600);
 }
 
-void loop() { 
-  // Read sensor values
-  Serial.println("teste");
+// 210~620 Valor dedo Medio
+// 1~210 valor rebaixado
+// 620~1023 valor extendido 
+void loop() {
+    for (int i = 0; i < 10; i++) {
+    Serial.println();
+  }
+
+  // Lê o valor do pino analógico A0.
+  int mindinho = analogRead(A0);
+  // Lê o valor do pino analógico A1.
+  int anelar = analogRead(A1);
+  // Lê o valor do pino analógico A2.
+  int meio = analogRead(A2);
+  // Lê o valor do pino analógico A3.
+  int indicador = analogRead(A3);
+  // Lê o valor do pino analógico A4.
+  int dedao = analogRead(A4);
+
+  if (mindinho)
+
+  delay(1000);
 }
